972d1e3f53701705eaaa329077f03417dfd81828
Christopher <christopher.diercksen@gmail.com>
2021-09-14 15:39:28 -0400

    commit tree 44a7faef231feeed55e4fa5381a7ef99a3b9661c

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+let moreCats = [];
+let lessCats = [];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+   moreCats = [...cats, "Broom"];
+   return moreCats;
+}
+
+function prependCat() {
+    moreCats = ["Arnold", ...cats];
+    return moreCats;
+}
+
+function removeLastCat() {
+    lessCats = cats.slice(0, 2);
+    return lessCats;
+}
+
+function removeFirstCat() {
+    lessCats = cats.slice(1);
+    return lessCats;
+}
